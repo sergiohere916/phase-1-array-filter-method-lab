97d8ea4aa75edbc57262fef7c8ce968ec2be40b2
Sergio Heredia <sergioheredia1996@gmail.com>
2023-08-04 14:24:04 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(inputList, value) {
+    const filteredList = inputList.filter(listElements => {
+        if (listElements.toUpperCase() === value.toUpperCase()) {
+            return listElements;
+        }
+    })
+    return filteredList;
+}
+
+function fuzzyMatch(inputList, value) {
+    const filteredList = inputList.filter(listElement => {
+       if (listElement.toUpperCase().indexOf(value.toUpperCase()) === 0) {
+        return listElement; 
+       }
+    })
+    return filteredList;
+}
+
+function matchName(inputList, value) {
+    const filteredList = inputList.filter(personObj => {
+        if (personObj.name.toUpperCase() === value.toUpperCase()) {
+            return personObj;
+        }
+    })
+    return filteredList; 
+}
